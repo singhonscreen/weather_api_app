d1f9e653d0643625c5fef2dbbbdc86f5ecec7024
Avadhesh Singh <avadhesh65@yahoo.com>
2021-08-14 12:35:04 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const App = () => {
 
   useEffect(() => {
     const fetchApi = async () => {
-      const url = `http://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&APPID=230ac2bd77b3b2471fcca16f13beb4a8`;
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&APPID=230ac2bd77b3b2471fcca16f13beb4a8`;
       const response = await fetch(url)
       const resJson = await response.json()
       setData(resJson.main)
